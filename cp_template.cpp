f4a782735b4b1602575237d31ed6fdbf7a832de3
cacteyy <m.wawrzyniaczek@gmail.com>
2024-06-23 23:04:36 +0200

    added a template

---
+++ cp_template.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define pii pair<int, int>
+#define pryes cout<<"YES"
+#define prno  cout<<"NO"
+#define vii vector<int>
+
+const int MOD = 1000000009; //1000000007
+
+void solve(){
+
+}
+
+int32_t main(){
+    cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+    int t = 1;
+    cin>>t; //comment if there is only one test per testcase
+    while(t--) solve();
+    return 0;
+}
